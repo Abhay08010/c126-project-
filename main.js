7f63ea675bfd050f3ec4d2ea925b4e78eb08a340
Abhay08010 <110532944+Abhay08010@users.noreply.github.com>
2023-03-06 16:03:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function summer(){
+    document.getElementById("image").src = "summer.png";
+}
+
+function winter(){
+    document.getElementById("image").src = "winter.png";
+}
+
+function spring(){
+    document.getElementById("image").src = "spring.png";
+}
+
+function autumn(){
+    document.getElementById("image").src = "autumn.png";
+}
